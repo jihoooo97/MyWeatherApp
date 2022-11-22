14a36851f3e149874447c48a0a2e2eecadc62994
Jiho <yjh7827@naver.com>
2022-11-22 09:49:21 +0900

    init

---
+++ MyWeatherApp/MyWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherAppApp.swift
+//  MyWeatherApp
+//
+//  Created by JihoMac on 2022/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
